55a1f4f56a60ee97157b7aaeb21063ccc809f61a
Tejeswar Reddy <tejeswarreddybeduthuri@gmail.com>
2026-09-06 22:21:33 +0530

    Delete backend directory

--- backend/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-DATA_DIR = os.path.join(BASE_DIR, "data")
-REPOSITORIES_DIR = os.path.join(DATA_DIR, "repositories")
